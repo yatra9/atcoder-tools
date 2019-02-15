8387fd43abc96a83206fc6d2fc184a1c683c004a
Kazuma Mikami <tyotyo3@gmail.com>
2019-02-16 05:14:26 +0900

    Updates

---
+++ precache-manifest.50a256decb547e33e36022459a43a2a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "79c6759e08761e9ebba558dbb0ce1fc5",
+    "url": "/atcoder-tools/static/media/README_TEMPLATE_SECTION.79c6759e.md"
+  },
+  {
+    "revision": "f6b8f4f3a6e0c67452f26f3f14744557",
+    "url": "/atcoder-tools/static/media/README.f6b8f4f3.md"
+  },
+  {
+    "revision": "3d8eb5560ad5e3c1d0cd",
+    "url": "/atcoder-tools/static/js/runtime~main.3d8eb556.js"
+  },
+  {
+    "revision": "bf9b2eb00cf7c44d2573",
+    "url": "/atcoder-tools/static/js/main.bf9b2eb0.chunk.js"
+  },
+  {
+    "revision": "cc9b71310dae43044b59",
+    "url": "/atcoder-tools/static/js/1.cc9b7131.chunk.js"
+  },
+  {
+    "revision": "bf9b2eb00cf7c44d2573",
+    "url": "/atcoder-tools/static/css/main.0893fd86.chunk.css"
+  },
+  {
+    "revision": "cc9b71310dae43044b59",
+    "url": "/atcoder-tools/static/css/1.3641563d.chunk.css"
+  },
+  {
+    "revision": "dfcc95957a3eea525807686c379c6623",
+    "url": "/atcoder-tools/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ importScripts("/atcoder-tools/workbox-v3.6.3/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/atcoder-tools/workbox-v3.6.3"});
 
 importScripts(
-  "/atcoder-tools/precache-manifest.c4ca52cdf7d478946d767cb757a26005.js"
+  "/atcoder-tools/precache-manifest.50a256decb547e33e36022459a43a2a9.js"
 );
 
 workbox.clientsClaim();
